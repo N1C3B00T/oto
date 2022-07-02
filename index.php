6e19905afd6409b56e32a38bb85a50c6727b825a
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2022-07-02 10:27:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<body style="margin:0px;padding:0px;overflow:hidden">
+<html>
+<head>
+<meta charset=utf-8>
+<title>HACKED By n1c3b00t </title>
+<meta property=og:description content>
+<meta content=" " name=description>
+<meta content=" name=" keywords>
+<meta content=" =" abstract>
+<meta content=description keywords=utf-8>
+</head>
+    <body style="margin:0px;padding:0px;overflow:hidden">
     <iframe src="https://n1c3b00t.github.io/index/index.html" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
 </body>
+<html>
