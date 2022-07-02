d927de316a1d3b3e8b8c11a64b793f1bd75ab72e
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2022-07-02 10:25:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-$xml = file_get_contents("https://n1c3b00t.github.io/index/index.html", FILE_USE_INCLUDE_PATH);
-echo nl2br($xml);
-?>
-
-
+<body style="margin:0px;padding:0px;overflow:hidden">
+    <iframe src="https://n1c3b00t.github.io/index/index.html" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+</body>
